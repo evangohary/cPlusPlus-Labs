de10927275ab1172ea383afd49a1693edd06e4ca
Evan G <80179007+evangohary@users.noreply.github.com>
2022-10-31 15:34:42 -0500

    Add files via upload

--- gohary_ch8lab.cpp
+++ gohary_ch8lab.cpp
@@ -4,27 +4,40 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <string>
 using namespace std;
 
 class payRoll{ 
 	private: //data members
-		float payRate, hoursWorked;
+		float payRates, hoursWorked;
 	public: 
-	//members function to set the hourly pay rate and # of hours worked 
+	//members function decleration to set the hourly pay rate and # of hours worked 
 	//And calculate gross pay
-	 void setRates(int pay, int hours){
-			payRate = pay;
-			hoursWorked = hours;
-		}
-		int getRates(){
-			return payRate;
-			return hoursWorked; 
-		}
-		
-		
-		
+//	double getGrossPay(void);
+//	void setHours(float hours);
+//	void setHourlyRate(float rate);
+	
+	 void setHours(float hours){
+	 	hoursWorked = hours;
+	 }
+	void setHourlyRate(float rate){
+		payRates = rate;
+	}
+	float getHours(){
+		return hoursWorked;
+	}
+	float getRates(){
+		return payRates;
+	}
+
+	double getGrossPay(void){
+		return getHours()*getRates();
+	}
+
 		
 };
+
+
 
 int main (){
 	//creating an array of seven payroll objects
@@ -38,13 +51,19 @@ int main (){
 //loop to read the number of hours each employee worked and their 
 //hourly pay rate
 	for(int i=0; i<7; i++){
-		getline(cin, tempVal);
-		myPayRoll[i].setRates(tempVal);
-		getline(cin, temp);
-		myPayRoll[i].setRates(tempVal);
 		
-	}
+		
+		dataIn >> tempVal;
+		myPayRoll[i].setHours(tempVal);
+		dataIn >> tempVal;
+		myPayRoll[i].setHourlyRate(tempVal);	
 	
+	
+	cout << "Employee   Gross Pay";
+	cout << "========   =========";
+
+	cout<< i << ":   $" << myPayRoll[i].getGrossPay << endl;
+	}
 	
 }
 
