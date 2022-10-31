1646ac102fb6b36d7e72137f0122509c5305dede
Evan G <80179007+evangohary@users.noreply.github.com>
2022-10-31 14:43:29 -0500

    Add files via upload

--- gohary_ch8lab.cpp
+++ gohary_ch8lab.cpp
@@ -1,30 +1,54 @@
 //chapter 8 lab - array of payroll
-
+//Nothing works yet
 
 #include <iostream>
 #include <iomanip>
 #include <fstream>
 using namespace std;
 
-class payRoll{
-	private:
-		double payRate, hoursWorked;
-	public:
-		
-		double grossPay(){
-			grossPay = payRate* hoursWorked;
-			return grossPay;
+class payRoll{ 
+	private: //data members
+		float payRate, hoursWorked;
+	public: 
+	//members function to set the hourly pay rate and # of hours worked 
+	//And calculate gross pay
+	 void setRates(int pay, int hours){
+			payRate = pay;
+			hoursWorked = hours;
 		}
+		int getRates(){
+			return payRate;
+			return hoursWorked; 
+		}
+		
+		
+		
+		
 };
 
 int main (){
-	const int size = 7;
-	int A[SIZE] = {};
+	//creating an array of seven payroll objects
+	payRoll myPayRoll[7];
+	float tempVal;
 	
-	
-	ifstream dataIn;
-	string line;
-	
+	ifstream dataIn;	
 	dataIn.open("payroll.txt");
+
+
+//loop to read the number of hours each employee worked and their 
+//hourly pay rate
+	for(int i=0; i<7; i++){
+		getline(cin, tempVal);
+		myPayRoll[i].setRates(tempVal);
+		getline(cin, temp);
+		myPayRoll[i].setRates(tempVal);
+		
+	}
+	
 	
 }
+
+
+
+
+
